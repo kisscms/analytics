716cb3e99e2919ce5938593a8172356e864622f7
Makis Tracend <makis.tracend@gmail.com>
2012-08-12 05:35:06 -0700

    Updating script inclusion

--- views/foot.php
+++ views/foot.php
@@ -1,7 +1,9 @@
-
-  <script>
-    var _gaq=[["_setAccount","<?=$config['analytics']['account']?>"],["_trackPageview"]];
-    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-    g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-    s.parentNode.insertBefore(g,s)}(document,"script"));
-  </script>
+<script type="text/javascript" data-type="require">
+	var _gaq=[["_setAccount","<?=$config['analytics']['account']?>"],["_trackPageview"]];
+	(function(d,t) {
+		var id = 'google-analytics'; if (d.getElementById(id)) {return;}
+		var js = d.createElement(t); js.type = 'text/javascript'; js.id = id; js.async = true;
+		js.src = ("https:"==location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";
+		var s = document.getElementsByTagName(t)[0]; s.parentNode.insertBefore(js, s);
+	})(document,"script");
+</script>
