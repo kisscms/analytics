61ace107768ed6203f15119ebcf318f1aa55e5cd
Makis Tracend <makis.tracend@gmail.com>
2013-06-26 21:34:27 -0700

    #1 using domain_name if not empty

--- views/foot.php
+++ views/foot.php
@@ -1,5 +1,8 @@
 <script type="text/javascript" data-type="require">
 	var _gaq=[["_setAccount","<?=$config['analytics']['account']?>"],["_trackPageview"]];
+<? if( !empty($config['analytics']['domain_name']) ){ ?>
+	_gaq.push(['_setDomainName', '<?=$config['analytics']['domain_name']?>']);
+<? } ?>
 	(function(d,t) {
 		var id = 'google-analytics'; if (d.getElementById(id)) {return;}
 		var js = d.createElement(t); js.type = 'text/javascript'; js.id = id; js.async = true;
