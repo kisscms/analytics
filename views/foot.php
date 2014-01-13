4e0067becd6ad89522046a44ea80fd75d63b2fd3
Makis Tracend <makis.tracend@gmail.com>
2014-01-12 20:10:37 -0800

    replacing embeded code with new analytics.js

--- views/foot.php
+++ views/foot.php
@@ -1,14 +1,10 @@
 <? if( !empty($config['analytics']['account']) ){ ?>
 <script type="text/javascript" data-type="require">
-	var _gaq=[["_setAccount","<?=$config['analytics']['account']?>"],["_trackPageview"]];
-<? if( !empty($config['analytics']['domain_name']) ){ ?>
-	_gaq.push(['_setDomainName', '<?=$config['analytics']['domain_name']?>']);
-<? } ?>
-	(function(d,t) {
-		var id = 'google-analytics'; if (d.getElementById(id)) {return;}
-		var js = d.createElement(t); js.type = 'text/javascript'; js.id = id; js.async = true;
-		js.src = ("https:"==location.protocol?"https://ssl":"http://www")+".google-analytics.com/ga.js";
-		var s = document.getElementsByTagName(t)[0]; s.parentNode.insertBefore(js, s);
-	})(document,"script");
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', '<?=$config['analytics']['account']?>', '<?=$config['analytics']['domain_name']?>');
+	ga('send', 'pageview');
 </script>
 <? } ?>
